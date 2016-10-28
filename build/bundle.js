8e2caa3cd8cb00398fc3462f66e0171115875258
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2016-10-29 09:43:05 +1100

    hello wordl

---
+++ build/bundle.js
@@ -0,0 +1,1 @@
+!function(o){function r(n){if(t[n])return t[n].exports;var e=t[n]={exports:{},id:n,loaded:!1};return o[n].call(e.exports,e,e.exports,r),e.loaded=!0,e.exports}var t={};return r.m=o,r.c=t,r.p="",r(0)}([function(o,r,t){t(1),function(){throw new Error('Cannot find module "tsx,ts"')}()},function(o,r){"use strict";console.log("Hello world")}]);
