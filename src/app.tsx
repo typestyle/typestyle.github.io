8edc5b260acddecef887dcee22368f9451fae30c
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2016-10-29 09:49:05 +1100

    hello world

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,9 @@
-console.log("Hello world")
+import { setupPage, normalize } from 'typestyle/csx';
+normalize();
+setupPage('#root');
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<div>Hello world</div>, document.getElementById('root'));
