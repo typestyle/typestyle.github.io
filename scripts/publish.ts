615e893bc991e7a8219dfab3c96a753eaece95fc
basarat <basaratali@gmail.com>
2016-10-29 19:27:01 +1100

    log err

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -5,7 +5,11 @@ const path = require('path');
 
 ghpages.publish(__dirname, {
   branch: 'master'
-}, () => {
+}, (err) => {
+  if (err) {
+    console.log('--publish failed!--', err)
+    return;
+  }
   console.log("--publish done--");
 });
 
