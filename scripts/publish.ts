42ee97b5cea54667dd87f82dfb6bc3348cd01556
basarat <basaratali@gmail.com>
2016-10-30 08:49:16 +1100

    simpler npm requirment

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -2,10 +2,20 @@ console.log("--running publish--");
 
 const ghpages = require('gh-pages');
 const path = require('path');
+const date = new Date();
 
 ghpages.publish(path.resolve(__dirname + '/../public'), {
+  message: `[ci skip] deployment (${date.getUTCFullYear()},${date.getUTCMonth()+1},${date.getUTCDate()},${date.getUTCHours()},${date.getUTCMinutes()})`,
+
+  /** Branch */
   branch: 'master',
-  repo: 'https://' + process.env.GH_TOKEN + '@github.com/typestyle/typestyle.github.io.git'
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/typestyle/typestyle.github.io.git',
+
+  /** User */
+  user: {
+    name: 'basarat',
+    email: 'basarat@example.com'
+  }
 }, (err) => {
   if (err) {
     console.log('--publish failed!--', err)
