9b7be9613737f28715ce561a5b6e6875e581f044
basarat <basaratali@gmail.com>
2016-10-29 19:38:00 +1100

    token

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -4,7 +4,8 @@ const ghpages = require('gh-pages');
 const path = require('path');
 
 ghpages.publish(__dirname, {
-  branch: 'master'
+  branch: 'master',
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/typestyle/typestyle.github.io.git'
 }, (err) => {
   if (err) {
     console.log('--publish failed!--', err)
