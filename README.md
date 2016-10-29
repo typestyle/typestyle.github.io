e94f6581c4a1234878099671723f9c3e243fb83e
basarat <basaratali+github@gmail.com>
2016-10-29 08:43:34 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# typestyle.github.io
-Website for TypeStyle
---
+++ publish.ts
@@ -0,0 +1,16 @@
+console.log("--running publish--");
+
+const ghpages = require('gh-pages');
+const path = require('path');
+
+ghpages.publish(__dirname, {
+  branch: 'master',
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/typestyle/typestyle.github.io.git'
+}, (err) => {
+  if (err) {
+    console.log('--publish failed!--', err)
+    return;
+  }
+  console.log("--publish done--");
+});
+
