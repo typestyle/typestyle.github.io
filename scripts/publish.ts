763782c4316bb9141d0cff18ab5037fb2f829cd9
basarat <basaratali@gmail.com>
2016-10-29 19:17:55 +1100

    give it a go

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,1 +1,11 @@
-console.log("running publish");
+console.log("--running publish--");
+
+const ghpages = require('gh-pages');
+const path = require('path');
+
+ghpages.publish(__dirname, {
+  branch: 'master'
+}, () => {
+  console.log("--publish done--");
+});
+
