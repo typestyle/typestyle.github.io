cb799aeaf72e02cc1d3c8f19d4a9e3ee77a79321
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2016-11-06 16:02:44 +1100

    work in progress

--- src/play/srcLoader.ts
+++ src/play/srcLoader.ts
@@ -7,3 +7,36 @@ export const setSource = (source: string) => {
     window.location.hash = hash;
     window.onhashchange = function() { window.location.hash = hash }
 }
+
+/**
+ * http://stackoverflow.com/a/11654596/390330
+ */
+function updateQueryString(key: string, value: string) {
+    let url = window.location.href;
+    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+        hash;
+
+    if (re.test(url)) {
+        if (typeof value !== 'undefined' && value !== null)
+            return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        else {
+            hash = url.split('#');
+            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+                url += '#' + hash[1];
+            return url;
+        }
+    }
+    else {
+        if (typeof value !== 'undefined' && value !== null) {
+            var separator = url.indexOf('?') !== -1 ? '&' : '?';
+            hash = url.split('#');
+            url = hash[0] + separator + key + '=' + value;
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+                url += '#' + hash[1];
+            return url;
+        }
+        else
+            return url;
+    }
+}
